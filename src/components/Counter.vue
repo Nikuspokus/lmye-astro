236d3b6c73dcabb40d673c3aa9b8837af219310a
NikusM <nicolas.marquillier@gmail.com>
2024-03-18 23:13:11 +0100

    adding tailwind

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue";
 
 const count = ref(0);
 const add = () => count.value++;
@@ -7,26 +7,44 @@ const subtract = () => count.value--;
 </script>
 
 <template>
-	<div class="counter">
-		<button @click="subtract">-</button>
-		<pre>{{ count }}</pre>
-		<button @click="add">+</button>
-	</div>
-	<div class="counter-message">
-		<slot />
-	</div>
+  <div class="counter">
+    <button class="btn btn-blue" @click="subtract">-</button>
+    <pre>{{ count }}</pre>
+    <button class="btn btn-blue" @click="add">+</button>
+  </div>
+  <div class="counter-message">
+    <slot />
+  </div>
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  >
+    Button
+  </button>
+
+  <!-- Extracting component classes: -->
+  <button class="btn btn-blue">Button</button>
 </template>
 
 <style>
 .counter {
-	display: grid;
-	font-size: 2em;
-	grid-template-columns: repeat(3, minmax(0, 1fr));
-	margin-top: 2em;
-	place-items: center;
+  display: grid;
+  font-size: 2em;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  margin-top: 2em;
+  place-items: center;
 }
 
 .counter-message {
-	text-align: center;
+  text-align: center;
+}
+
+.btn {
+  @apply font-bold py-2 px-4 rounded;
+}
+.btn-blue {
+  @apply bg-blue-500 text-white;
+}
+.btn-blue:hover {
+  @apply bg-blue-700;
 }
 </style>
